e12f693a0b6b1148924eac36de0ad0c467767615
Victor Manuel <mbvictorsantos@gmail.com>
2022-07-26 18:38:36 -0300

    feat: Adiciona rotas de administrador.

--- src/services/router.tsx
+++ src/services/router.tsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
 import Home from '../pages/Home';
+import Login from '../pages/Login';
+
+import Dashboard from '../pages/Admin/Dashboard';
 
 const AdminRoute = ({ children = null }: { children?: JSX.Element | null }) => {
-  const { access_token } = { access_token: null };
+  const accessToken = localStorage.getItem('access_token');
 
-  if (!access_token) {
+  if (!accessToken) {
     return <Navigate to="/login" replace />;
   }
 
@@ -17,7 +20,9 @@ const Router = () => (
     <Routes>
       <Route path="/" element={<Home />} />
 
-      <Route path="/login" element={<div>Login</div>} />
+      <Route path="/login" element={<Login />} />
+
+      <Route path="/admin/dashboard" element={<AdminRoute><Dashboard /></AdminRoute>} />
 
       <Route path="*" element={<div>NotFoundPage</div>} />
     </Routes>
