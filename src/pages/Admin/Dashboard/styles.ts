36e446b4bf114a155f144edc8f64e234ab330538
Victor Manuel <mbvictorsantos@gmail.com>
2022-07-26 18:49:17 -0300

    feat: Cria página de Dashboard para administradores.

---
+++ src/pages/Admin/Dashboard/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const ContainerPage = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+`;
+
+export const ContainerTable = styled.div`
+  height: 600px;
+  width: 80%;
+`;
