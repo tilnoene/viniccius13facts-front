f27371aeedb6e97f12ac3d30db28eaa878eecf30
Victor Manuel <mbvictorsantos@gmail.com>
2022-07-26 22:44:14 -0300

    feat: Adiciona userProvider para o Login.

--- src/services/router.tsx
+++ src/services/router.tsx
@@ -4,9 +4,10 @@ import Home from '../pages/Home';
 import Login from '../pages/Login';
 
 import Dashboard from '../pages/Admin/Dashboard';
+import UserProvider, { useUser } from '../context/UserContext';
 
 const AdminRoute = ({ children = null }: { children?: JSX.Element | null }) => {
-  const accessToken = localStorage.getItem('access_token');
+  const accessToken = useUser();
 
   if (!accessToken) {
     return <Navigate to="/login" replace />;
@@ -20,9 +21,25 @@ const Router = () => (
     <Routes>
       <Route path="/" element={<Home />} />
 
-      <Route path="/login" element={<Login />} />
+      <Route
+        path="/login"
+        element={
+          <UserProvider>
+            <Login />
+          </UserProvider>
+        }
+      />
 
-      <Route path="/admin/dashboard" element={<AdminRoute><Dashboard /></AdminRoute>} />
+      <Route
+        path="/admin/dashboard"
+        element={
+          <UserProvider>
+            <AdminRoute>
+              <Dashboard />
+            </AdminRoute>
+          </UserProvider>
+        }
+      />
 
       <Route path="*" element={<div>NotFoundPage</div>} />
     </Routes>
