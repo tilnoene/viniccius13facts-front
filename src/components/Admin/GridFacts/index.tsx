a946f5e38186cc84c63968412ba2394eff9c0e2a
Victor Manuel <mbvictorsantos@gmail.com>
2022-10-24 21:26:36 -0300

    feat: Adiciona opção para o modal de edição com um switch

--- src/components/Admin/GridFacts/index.tsx
+++ src/components/Admin/GridFacts/index.tsx
@@ -10,7 +10,14 @@ import { toastOptions } from '../../../services/utils';
 
 import DoneAllIcon from '@mui/icons-material/DoneAll';
 import ClearIcon from '@mui/icons-material/Clear';
-import { Box, Modal } from '@mui/material';
+import {
+  Box,
+  FormControlLabel,
+  InputAdornment,
+  Modal,
+  Switch,
+  TextField,
+} from '@mui/material';
 import FactQuote from '../../FactQuote';
 import Button from '../../Button';
 import { useUser } from '../../../context/UserContext';
@@ -20,8 +27,7 @@ const style = {
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  width: 'fit-content',
-  maxWidth: '400px',
+  width: window.innerWidth >= 420 ? '420px' : '300px',
   bgcolor: 'background.paper',
   boxShadow: 24,
   p: 6,
@@ -41,17 +47,40 @@ const useStyles = makeStyles({
 const GridFacts = ({
   facts,
   setFacts,
+  handlePrev,
+  handleNext,
 }: {
   facts: Fact[];
   setFacts: (facts: Fact[]) => void;
+  handlePrev: (page: number) => void;
+  handleNext: (page: number) => void;
 }) => {
   const classes = useStyles();
   const { accessToken } = useUser();
 
   const [currentFact, setCurrentFact] = useState<Fact | undefined>();
+  const [editMode, setEditMode] = useState<boolean>(false);
   const [openModal, setOpenModal] = useState<boolean>(false);
   const handleOpenModal = () => setOpenModal(true);
-  const handleCloseModal = () => setOpenModal(false);
+  const handleCloseModal = () => {
+    setOpenModal(false);
+    setEditMode(false);
+  };
+
+  const [username, setUsername] = useState<string>('');
+  const [message, setMessage] = useState<string>('');
+
+  const handleChangeUsername = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => {
+    setUsername(event.target.value);
+  };
+
+  const handleChangeMessage = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => {
+    setMessage(event.target.value);
+  };
 
   const onClickCell = (fact?: Fact) => {
     if (!fact) {
@@ -59,6 +88,10 @@ const GridFacts = ({
     }
 
     setCurrentFact(fact);
+
+    setUsername(fact?.author?.username || '');
+    setMessage(fact?.message || '');
+
     handleOpenModal();
   };
 
@@ -118,6 +151,60 @@ const GridFacts = ({
       });
   };
 
+  const handleEditMode = () => {
+    setEditMode(!editMode);
+
+    setUsername(currentFact?.author?.username || '');
+    setMessage(currentFact?.message || '');
+  };
+
+  const handleEdit = () => {
+    if (!currentFact) {
+      toast.error('Nenhum fato foi selecionado!', toastOptions);
+      return;
+    }
+
+    if (username.length === 0) {
+      toast.error('Username inválido!', toastOptions);
+      return;
+    }
+
+    if (message.length === 0) {
+      toast.error('Fato inválido!', toastOptions);
+      return;
+    }
+
+    api
+      .patch(
+        `/facts/${currentFact.id}`,
+        {
+          message,
+          author: {
+            username,
+            url: `https://twitter.com/${username}`,
+          },
+        },
+        {
+          headers: {
+            Authorization: accessToken,
+          },
+        },
+      )
+      .then((response: any) => {
+        toast.success('Fato editado com sucesso!', toastOptions);
+
+        setUsername('');
+        setMessage('');
+
+        setCurrentFact(response.data);
+
+        setEditMode(false);
+      })
+      .catch(() => {
+        toast.error('Ocorreu um erro ao editar o fato!', toastOptions);
+      });
+  };
+
   const columns: GridColDef[] = [
     {
       field: 'author',
@@ -126,7 +213,7 @@ const GridFacts = ({
       disableColumnMenu: true,
       renderCell: (params: GridValueGetterParams) => (
         <Cell onClick={() => onClickCell(params.row)}>
-          {params.row?.author.username}
+          {params.row?.author?.username}
         </Cell>
       ),
     },
@@ -190,9 +277,65 @@ const GridFacts = ({
         aria-describedby="modal-submit-fact-description"
       >
         <Box sx={style}>
-          {currentFact && <FactQuote fact={currentFact} />} <br />
-          <Button onClick={() => handleApprove(currentFact)}>Aprovar</Button>
-          <Button onClick={() => handleDeny(currentFact)}>Rejeitar</Button>
+          <FormControlLabel
+            control={
+              <Switch
+                checked={editMode}
+                onChange={handleEditMode}
+                inputProps={{ 'aria-label': 'controlled' }}
+              />
+            }
+            label="Modo de edição"
+          />
+
+          {editMode ? (
+            <div
+              style={{ display: 'flex', gap: '16px', flexDirection: 'column' }}
+            >
+              <TextField
+                id="outlined-message-flexible"
+                label="Username"
+                placeholder="username"
+                value={username}
+                onChange={event => handleChangeUsername(event)}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">@</InputAdornment>
+                  ),
+                }}
+                required
+                inputProps={{
+                  maxLength: 32,
+                }}
+                autoComplete="off"
+              />
+
+              <TextField
+                id="outlined-message-flexible"
+                label="Fato"
+                placeholder="Escreva seu fato..."
+                multiline
+                required
+                rows={4}
+                value={message}
+                onChange={event => handleChangeMessage(event)}
+                inputProps={{
+                  maxLength: 256,
+                }}
+                autoComplete="off"
+              />
+
+              <Button onClick={() => handleEdit()}>Salvar</Button>
+            </div>
+          ) : (
+            <>
+              {currentFact && <FactQuote fact={currentFact} withId />} <br />
+              <Button onClick={() => handleApprove(currentFact)}>
+                Aprovar
+              </Button>
+              <Button onClick={() => handleDeny(currentFact)}>Rejeitar</Button>
+            </>
+          )}
         </Box>
       </Modal>
     </ContainerGrid>
